169f924261ace8c869d39865cb39d79691bed032
gregorybchris <gregorybchris@gmail.com>
2020-12-28 04:37:43 -0800

    Factor out graph creation

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,14 @@
-import { createGraph, fetchGraph } from "./simulation/graph-factory.js";
+import GraphFactory from "./simulation/graph-factory.js";
 import { GraphFormats, logRepr, logSummary } from "./simulation/graph-logging.js";
 import Orchestrator from "./orchestration/orchestrator.js";
 
-// const graph = await fetchGraph();
-const graph = createGraph();
+// GraphFactory.fetchSimple().then((graph) => {
+GraphFactory.fetch().then((graph) => {
+  // logRepr(graph, GraphFormats.MATRIX);
+  // logRepr(graph, GraphFormats.LIST);
+  logSummary(graph);
 
-// logRepr(graph, GraphFormats.MATRIX);
-// logRepr(graph, GraphFormats.LIST);
-logSummary(graph);
-
-const canvas = document.getElementById("canvas");
-const orchestrator = new Orchestrator(canvas, graph);
-orchestrator.start();
+  const canvas = document.getElementById("canvas");
+  const orchestrator = new Orchestrator(canvas, graph);
+  orchestrator.start();
+});
--- scripts/simulation/graph-factory.js
+++ scripts/simulation/graph-factory.js
@@ -1,41 +1,47 @@
 import Graph from "../simulation/graph.js";
 
-const createGraph = () => {
-  const graph = new Graph();
-  graph.addNode("a", { size: "big" });
-  graph.addNode("b", { size: "small" });
-  graph.addNode("c", { size: "small" });
-  graph.addNode("d", { size: "big" });
-  graph.addNode("e", { size: "small" });
-  graph.addNode("f", { size: "small" });
+class GraphFactory {
+  static fetch = async () => {
+    const dataUrl = "https://chrisgregory.blob.core.windows.net/datasets/les-miserables.json";
+    const graphData = await fetch(dataUrl, {
+      mode: "cors",
+      headers: { "Access-Control-Allow-Origin": "*" },
+    }).then((response) => response.json());
 
-  ["b", "c"].forEach((id) => {
-    graph.addEdge("a", id);
-  });
-  ["a", "b", "e", "f"].forEach((id) => {
-    graph.addEdge("d", id);
-  });
+    const graph = new Graph();
+    const indexMap = {};
+    graphData.nodes.forEach((node, i) => {
+      indexMap[node.id] = i;
+      graph.addNode(i, { name: node.id, group: node.group });
+    });
+    graphData.links.forEach((edge) => {
+      graph.addEdge(indexMap[edge.source], indexMap[edge.target], false, edge.value);
+    });
+    return graph;
+  };
 
-  return graph;
-};
+  static fetchSimple = async () => {
+    return new Promise((resolve, reject) => resolve(GraphFactory.createSimple()));
+  };
 
-const fetchGraph = async () => {
-  const dataUrl = "https://chrisgregory.blob.core.windows.net/datasets/les-miserables.json";
-  const graphData = await fetch(dataUrl, {
-    mode: "cors",
-    headers: { "Access-Control-Allow-Origin": "*" },
-  }).then((response) => response.json());
+  static createSimple = () => {
+    const graph = new Graph();
+    graph.addNode("a", { size: "big" });
+    graph.addNode("b", { size: "small" });
+    graph.addNode("c", { size: "small" });
+    graph.addNode("d", { size: "big" });
+    graph.addNode("e", { size: "small" });
+    graph.addNode("f", { size: "small" });
 
-  const graph = new Graph();
-  const indexMap = {};
-  graphData.nodes.forEach((node, i) => {
-    indexMap[node.id] = i;
-    graph.addNode(i, { name: node.id, group: node.group });
-  });
-  graphData.links.forEach((edge) => {
-    graph.addEdge(indexMap[edge.source], indexMap[edge.target], false, edge.value);
-  });
-  return graph;
-};
+    ["b", "c"].forEach((id) => {
+      graph.addEdge("a", id);
+    });
+    ["a", "b", "e", "f"].forEach((id) => {
+      graph.addEdge("d", id);
+    });
 
-export { createGraph, fetchGraph };
+    return graph;
+  };
+}
+
+export default GraphFactory;
